6ccba82fdda13cfa496dc20c8b1adf979d40619f
thotasahithi <66264537+thotasahithi@users.noreply.github.com>
2020-06-20 15:42:42 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,106 @@
+var root=document.getElementById('main');
+
+var div1=document.createElement('div');
+div1.classList.add("sub")
+root.appendChild(div1);
+
+ var image=document.createElement('img');
+ image.src="1.jpg";
+ image.classList.add('img');
+ div1.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="sahithi";
+div1.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="student"
+div1.classList.add("p")
+div1.appendChild(para);
+
+var para1=document.createElement('p');
+para1.textContent="thotasahithi2001@gmail.com"
+div1.classList.add("p")
+div1.appendChild(para1);
+
+var para2=document.createElement('p');
+para2.textContent="1249783254"
+div1.classList.add("p")
+div1.appendChild(para2);
+
+var a=document.createElement('a');
+a.href="article.html"
+a.textContent="resume"
+div1.classList.add("a")
+div1.appendChild(a);
+
+
+
+var div2=document.createElement('div');
+div2.classList.add("sub")
+root.appendChild(div2);
+
+ var image=document.createElement('img');
+ image.src="1.jpg";
+ image.classList.add('img');
+ div2.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="sahi";
+div2.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="student"
+para.classList.add("p")
+div2.appendChild(para);
+
+var para1=document.createElement('p');
+para1.textContent="thotasahi@gmail.com"
+div2.classList.add("p")
+div2.appendChild(para1);
+
+var para2=document.createElement('p');
+para2.textContent="1249783254"
+div2.classList.add("p")
+div2.appendChild(para2);
+
+var a=document.createElement('a');
+a.href="article.html"
+a.textContent="resume"
+div2.classList.add("a")
+div2.appendChild(a);
+
+
+var div3=document.createElement('div');
+div3.classList.add("sub")
+root.appendChild(div3);
+
+ var image=document.createElement('img');
+ image.src="1.jpg";
+ image.classList.add('img');
+ div3.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="sahi";
+div3.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="student"
+para.classList.add("p")
+div3.appendChild(para);
+
+var para1=document.createElement('p');
+para1.textContent="thotasahi@gmail.com"
+div3.classList.add("p")
+div3.appendChild(para1);
+
+var para2=document.createElement('p');
+para2.textContent="1249783254"
+div3.classList.add("p")
+div3.appendChild(para2);
+
+var a=document.createElement('a');
+a.href="article.html"
+a.textContent="resume"
+div3.classList.add("a")
+div3.appendChild(a);
